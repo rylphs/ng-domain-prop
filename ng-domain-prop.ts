393c092f01fc09d2cad4dbfd34b6ce3d28b16ad0
Raphael Couto Ferreira - SUPCD/CDTEC/CDABH <07125220690@serpro-1528571.(none)>
2016-11-07 16:46:21 -0200

    Removed link to removed file.

--- ng-domain-prop.ts
+++ ng-domain-prop.ts
@@ -1,5 +1,4 @@
 export * from './src/domain-property/domain-property.decorator';
 export * from './src/domain-property/domain-value';
 export * from './src/domain-property/uses-domain-properties.decorator';
-export * from './src/forms/validate-domain-values.decorator';
 export * from './src/forms/validate-domain.directive';
